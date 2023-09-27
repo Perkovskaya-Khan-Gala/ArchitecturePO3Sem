5267de942e512b4fb9b52953b198a2edfe0c02c1
Galka <hgalala@mail.ru>
2023-09-27 10:49:36 +0700

    Prove some wordspelling mistakes

--- src/OCP/SpeedCalculate.java
+++ src/OCP/SpeedCalculate.java
@@ -1,13 +1,13 @@
 package OCP;
 
 public class SpeedCalculate {
-    public double cclaculateAllowedSpeed(Vehicle vehicle){
-        if (vehicle.getType().equalsIgnoreCase("Car")){
+    public double calculateAllowedSpeed(Vehicle vehicle) {
+        if (vehicle.getType().equalsIgnoreCase("Car")) {
             return vehicle.getMaxSpeed() * 0.8;
-        }
-        else if (vehicle.getType().equalsIgnoreCase("Bus")){
+        } else if (vehicle.getType().equalsIgnoreCase("Bus")) {
             return vehicle.getMaxSpeed() * 0.5;
         }
+        return -1;
 
     }
 }
